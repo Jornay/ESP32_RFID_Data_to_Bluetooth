1acf9bfadb343847df90d6843e0e58ee37f8516e
Lucas de Abreu Furtado Garcia <52716819+Jornay@users.noreply.github.com>
2023-09-23 11:31:33 -0300

    feat: LED stays on for 1 second - stay on until another card is detected

--- esp32_RFID_Data_to_Bluetooth.ino
+++ esp32_RFID_Data_to_Bluetooth.ino
@@ -4,101 +4,70 @@
 
 #define SDA_PIN 21
 #define SCL_PIN 22
-#define LED_PIN 13 // Pino do LED
+#define LED_PIN 18 // Pino do LED
 
 Adafruit_PN532 nfc(SDA_PIN, SCL_PIN);
-const char *pin = "1234";  // Altere isso para um PIN mais seguro.
+const char *pin = "1234";  // Senha para conexão Bluetooth.
 
-//------------------BLUETOOTH-------------------------//
 String device_name = "ESP32-BT-Slave";
 BluetoothSerial SerialBT;
-//----------------------------------------------------------//
 
 void setup(void) {
   Serial.begin(115200);
   Serial.println("Hello! NFC reader with ESP32");
-
-  //------------------BLUETOOTH-------------------------//
-  SerialBT.begin(device_name);  // Inicializa o Bluetooth com o nome do dispositivo
-
+  SerialBT.begin(device_name);
+  
   #ifdef USE_PIN
   SerialBT.setPin(pin);
   Serial.println("Using PIN");
   pinMode(19, OUTPUT);
   #endif
-  //---------------------------------------------------//
-
-  pinMode(LED_PIN, OUTPUT); // Configura o pino do LED como saída
 
   nfc.begin();
   uint32_t versiondata = nfc.getFirmwareVersion();
 
-  nfc.SAMConfig();  // Configura a placa para ler tags NFC
+  nfc.SAMConfig();
   Serial.println("Waiting for an NFC card ...");
+  
+  pinMode(LED_PIN, OUTPUT); // Configura o pino do LED como saída
+  digitalWrite(LED_PIN, LOW); // Desliga o LED inicialmente
 }
-
-// Variáveis para armazenar o UID do cartão anterior
-uint8_t prevUid[7] = {0, 0, 0, 0, 0, 0, 0};
-uint8_t prevUidLength = 0;
 
 void loop(void) {
   uint8_t success;
-  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer para armazenar o UID retornado
-  uint8_t uidLength;                        // Comprimento do UID (4 ou 7 bytes dependendo do tipo de cartão ISO14443A)
+  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };
+  uint8_t uidLength;
+  char uidCodes[64] = "";
 
-  // Armazena os códigos lidos em uma matriz de caracteres
-  char uidCodes[64] = ""; // Tamanho máximo da string
-
-  // Aguarda a detecção de um cartão NFC
   success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
 
   if (success) {
     Serial.println("Found an NFC card!");
 
-    // Display some basic information about the card
     Serial.print("UID Length: ");
     Serial.print(uidLength, DEC);
     Serial.println(" bytes");
     Serial.print("UID Value: ");
+    
     for (uint8_t i = 0; i < uidLength; i++) {
       Serial.print(" 0x");
       Serial.print(uid[i], HEX);
-
-      // Adiciona o código lido à variável uidCodes
+      
       char hexCode[3];
       sprintf(hexCode, "%02X", uid[i]);
       strcat(uidCodes, hexCode);
     }
+    
     Serial.println("");
-
-    // Envia a variável uidCodes pelo Bluetooth
     SerialBT.println("RFID Codes:");
     SerialBT.println(uidCodes);
-
-    // Verifica se o cartão atual é o mesmo que o cartão anterior
-    if (isSameCard(uid, uidLength)) {
-      // Acende o LED e não aguarda um segundo
-      digitalWrite(LED_PIN, HIGH);
-    } else {
-      // Caso contrário, aguarda 1 segundo antes de desligar o LED
-      delay(1000);
-      digitalWrite(LED_PIN, LOW);
-    }
+    
+    // Acende o LED quando um cartão é detectado
+    digitalWrite(LED_PIN, HIGH);
+    delay(1000); // Mantém o LED aceso por 1 segundo
+    
+    // Desliga o LED
+    digitalWrite(LED_PIN, LOW);
+    delay(1000); // Aguarda 1 segundo antes de continuar
   }
-}
-
-// Função para verificar se o cartão atual é o mesmo que o cartão anterior
-bool isSameCard(uint8_t newUid[], uint8_t newUidLength) {
-  if (newUidLength != prevUidLength) {
-    return false;
-  }
-
-  for (int i = 0; i < newUidLength; i++) {
-    if (newUid[i] != prevUid[i]) {
-      return false;
-    }
-  }
-
-  // Se chegamos até aqui, os cartões são os mesmos
-  return true;
 }
